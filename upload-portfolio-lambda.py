61ba6d074639bf800141cd105c532c6c66c77691
bradydeclan <bradydeclan@gmail.com>
2019-01-10 17:03:34 +0000

    Use SNS to notify when portfolio is deployed

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,20 +6,29 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:eu-west-1:908220430595:deployPortfolioTopic')
 
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-    #s3 = boto3.resource('s3')
-    portfolio_bucket = s3.Bucket('portfolio.decbrady.com')
-    build_bucket = s3.Bucket('portfoliobuild.decbrady.com')
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        #s3 = boto3.resource('s3')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        portfolio_bucket = s3.Bucket('portfolio.decbrady.com')
+        build_bucket = s3.Bucket('portfoliobuild.decbrady.com')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                  ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print "Job done!"
+        topic.publish(Subject="Portfolio Deployed", Message = "This is a message to let you know that your recent update has been deployed")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message = "The Portfolio was NOT deployed successfully!")
+        raise
     return 'Hello from Lambda'
